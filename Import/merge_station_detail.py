d0cbc3a5aad644342586fe2cd33fe244e106f5e4
vejuhust <vejuhust@gmail.com>
2014-07-28 00:00:45 +0800

    Add merge_route in merge_station_detail.py as a trade-off
    Because raw data / external providers are not perfect, they may have
    wrong/incomplete data.

--- Import/merge_station_detail.py
+++ Import/merge_station_detail.py
@@ -7,7 +7,10 @@ import json
 
 
 station_detail_filename = "../Data/External/station_detail.json"
+line_route_filename = "Merged/line_route.json"
+
 merged_filename = "Merged/station_all.json"
+
 
 def load_json(filename):
     file = open(filename, 'r')
@@ -23,7 +26,7 @@ def save_file(filename, data):
 
 
 # grep NoteGuid ../Data/External/station_detail.json | sort | uniq | wc -l
-# Result: 699
+# Result: 703
 def merge_detail(raw, data):
     for item in raw:
         if "call_error" not in item:
@@ -40,13 +43,39 @@ def merge_detail(raw, data):
                         "code" : code.upper().strip(),
                         "name" : station["Name"].strip(),
                         "geo_side" : station["Direct"].strip(),
-                        "geo_road" : address
+                        "geo_road" : address,
                     }
     return data
 
+
+# if station_detail.json doesn't cover all the stations, get basic info of the left from line_route.json
+def merge_route(routes, data):
+    for guid in routes:
+        route = routes[guid]
+        stations = route["list"]["StandInfo"]
+        for station in stations:
+            code = station["SCode"]
+            if code not in data:
+                print route["list"]["LName"], route["list"]["LDirection"], route["list"]["LGUID"]
+                data[code] = {
+                    "code" : code.upper().strip(),
+                    "name" : station["SName"].strip(),
+                    "geo_side" : "",
+                    "geo_road" : "",
+                }
+    return data
+
+
 if __name__ == '__main__':
-    raw = load_json(station_detail_filename)
     data = {}
+    # From station_detail.json
+    raw = load_json(station_detail_filename)
     merge_detail(raw, data)
-    print len(data)
+    print "merge_detail", len(data)
+    # From line_route.json
+    line_route = load_json(line_route_filename)
+    merge_route(line_route, data)
+    print "merge_route", len(data)
+    # Output
     save_file(merged_filename, data)
+
